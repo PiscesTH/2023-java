afc6e9f6b59e2de1f03bbf5813982eebd87696aa
PiscesTH <kareme007@gmail.com>
2023-10-07 21:16:49 +0900

    10.7 home

--- src/main/java/com/green/self/ScannerExSelfComple.java
+++ src/main/java/com/green/self/ScannerExSelfComple.java
@@ -8,35 +8,34 @@ public class ScannerExSelfComple {
         System.out.print("1~20 사이 숫자를 하나 입력해주세요. : ");
         //String s = scan.nextLine();
         //int x = Integer.parseInt(s);
-        int x = scan.nextInt();
+        int goal = scan.nextInt();
         System.out.println("\n\n\n\n");
 
-        while (x > 20 || x < 1) {
+        while (goal > 20 || goal < 1) {
             System.out.print("1~20 사이 숫자를 다시 입력해주세요. : ");
             //String s1 = scan.nextLine();
             //s = s1;
-            x = scan.nextInt();
+            goal = scan.nextInt();
             System.out.println("\n\n\n\n");
 
         }
         //처음 정답 입력 문구 "정답을 입력해주세요 : " - 1번만 출력
         //오답 이후 정답 입력 문구 " 정답을 다시 입력해주세요 : " - 4번 출력
-        int i = 0;
-        while (i < 5) {
-            i++;
-            System.out.print(i == 1 ? "정답을 입력해주세요 : " : "정답을 다시 입력해주세요 : ");
-            //String a = scan.nextLine();
-            int y = scan.nextInt();
-            if (i == 5 && x != y) {
-                System.out.println("기회가 모두 소진되었습니다.");
-            } else {
-                if (x == y) {
-                    System.out.println("정답입니다.");
-                    break;
-                } else {
-                    System.out.printf("%s\n", x > y ? "Up" : "Down");
-                }
+        int tryTimes = 0;
+        final int OPPORTUNITY = 5;
+        while (tryTimes < OPPORTUNITY) {
+            tryTimes++;
+            System.out.print(tryTimes == 1 ? "정답을 입력해주세요 : " : "정답을 다시 입력해주세요 : ");
+            int answer = scan.nextInt();
+            if (goal == answer) {
+                System.out.println("정답입니다.");
+                break;
             }
+            if (tryTimes == OPPORTUNITY) {
+                System.out.println("기회가 모두 소진되었습니다.");
+                break;
+            }
+            System.out.printf("%s\n", goal > answer ? "Up" : "Down");
         }
     }
 }
