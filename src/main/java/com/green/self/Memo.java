d1b16141b8b9531c38c4addddae741c626473d15
TH <kareme007@gmail.com>
2023-10-10 15:07:36 +0900

    10.10

--- src/main/java/com/green/self/Memo.java
+++ src/main/java/com/green/self/Memo.java
@@ -3,6 +3,9 @@ package com.green.self;
 public class Memo {
     /*
     notion : 학습 내용 정리 추천.
-
+    2주마다 일지 작성. >> notion 참고.
+    9.21 부터 1주차. ~ 9. 29 >> 2주차
+    10.13 까지 >> 4주차
+    github commit message 변경용 수정
     */
 }
