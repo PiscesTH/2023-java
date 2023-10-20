d8a12d4e2d341ecf631a4b04fec0742e62553e32
TH <kareme007@gmail.com>
2023-10-20 17:52:56 +0900

    23.10.20.

--- src/main/java/com/green/day17/ch7/PolyArgumentTestVer2.java
+++ src/main/java/com/green/day17/ch7/PolyArgumentTestVer2.java
@@ -7,6 +7,96 @@ public class PolyArgumentTestVer2 {
     }
 }
 
+class PolyArgumentTestVer2_2 {
+    public static void main(String[] args) {
+        Tv2 tv = new Tv2();
+        System.out.println(tv);     //name : Tv, price : 100, bonusPoint : 10
+        Computer2 com = new Computer2();
+        System.out.println(com);    //name : Computer, price : 200, bonusPoint : 20
+        Audio2 audio = new Audio2();
+        System.out.println(audio);    //name : Audio, price : 500, bonusPoint : 50
+    }
+}
+
+class PolyArgumentTestVer2_3 {
+    public static void main(String[] args) {
+        Buyer2 buyer = new Buyer2();
+        buyer.buy(new Tv2());
+        buyer.buy(new Computer2());
+        buyer.buy(new Tv2());
+        buyer.buy(new Tv2());
+        buyer.buy(new Computer2());
+        buyer.buy(new Computer2());
+        buyer.buy(new Computer2());
+        buyer.buy(new Computer2());
+        buyer.buy(new Computer2());
+        buyer.buy(new Audio2());
+        buyer.buy(new Audio2());
+        System.out.println("---------------------");
+        buyer.summary(); //총 구매금액
+        System.out.println("---------------------");
+        buyer.getPurchaseList();
+    }
+}
+
+class Buyer2 {   //money, bonusPoint, Product2 주소값 10개 저장 가능
+    private int money;
+    private int bonusPoint;
+    private Product2[] pArr;
+    private int index;
+    private final int iMoney;
+
+    public Buyer2() {
+        money = 10000;
+        bonusPoint = 0;
+        pArr = new Product2[10];
+        iMoney = money;
+    }
+
+    public void buy(Product2 product2) {
+        if (index == pArr.length) {
+            System.out.println("구매 불가");
+            return;
+        }
+        int price = product2.getPrice();
+        int bonusPoint = product2.getBonusPoint();
+        if (money < price) {
+            System.out.println("잔액 부족");
+            return;
+        }
+        money -= price;
+        this.bonusPoint += bonusPoint;
+        pArr[index++] = product2;
+        System.out.printf("%s 구매 완료\n", product2.getName());
+    }
+
+    public void summary() {
+        int used = iMoney - money;
+        System.out.printf("구매 금액 : %d만원, 남은 금액 : %d, 포인트 : %d\n", used, money, bonusPoint);
+    }
+
+    public void getPurchaseList() {
+        int comCnt = 0, tvCnt = 0 , auidoCnt = 0;
+        for (int i = 0; i < pArr.length; i++) {
+            if (pArr[i] == null) {
+                break;
+            }
+            switch (pArr[i].getName()) {
+                case "Computer":
+                    comCnt++;
+                    break;
+                case "Tv":
+                    tvCnt++;
+                    break;
+                case "Audio":
+                    auidoCnt++;
+                    break;
+            }
+        }
+        System.out.printf("컴퓨터 : %d대, Tv : %d대, 오디오 : %d대\n", comCnt, tvCnt, auidoCnt);
+    }
+}
+
 class Product2 {
     private String name;
     private int price;
@@ -18,8 +108,38 @@ class Product2 {
         bonusPoint = price / 10;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getBonusPoint() {
+        return bonusPoint;
+    }
+
     @Override
     public String toString() {
-        return String.format("%s. price : %d, bonusPoint : %d", name, price, bonusPoint);
+        return String.format("name : %s, price : %d, bonusPoint : %d", name, price, bonusPoint);
+    }
+}
+
+class Tv2 extends Product2 {
+    public Tv2() {
+        super("Tv", 100);
+    }
+}
+
+class Computer2 extends Product2 {
+    public Computer2() {
+        super("Computer", 200);
+    }
+}
+
+class Audio2 extends Product2 {
+    public Audio2() {
+        super("Audio", 500);
     }
 }
