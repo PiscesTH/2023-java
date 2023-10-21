8cfde6a5f31f9d9ff9c2dd0894673f84c33286e0
PiscesTH <kareme007@gmail.com>
2023-10-21 14:31:47 +0900

    23.10.21. Home

--- src/main/java/com/green/self/SelfPrac4.java
+++ src/main/java/com/green/self/SelfPrac4.java
@@ -4,100 +4,155 @@ import java.util.Scanner;
 
 public class SelfPrac4 {
     public static void main(String[] args) {
-        int grade = 1;
-        Scanner scan = new Scanner(System.in);
-        System.out.printf("현재등급 : %d\n", grade);
+        Character c1 = new Character();
+        System.out.println(c1.getInventory().getName());
+        System.out.println(c1.getInventory().getCategory());
+
+    }
+}
+
+class Forge {
+    private final Scanner scan = new Scanner((System.in));
+    private Item characterItem;
+
+
+    public Forge(Character character) {
+        characterItem = character.getInventory();
+        System.out.printf("선택 된 장비 : %s\n", characterItem.getName());
+    }
+
+    public void startUpgrade() {
+        System.out.printf("현재등급 : %d\n", characterItem.getGrade());
         System.out.print("Upgrade / Stop : ");
         String input = scan.nextLine();
+        int tmpGrade = characterItem.getGrade();
         forge:
         while (true) {
             switch (input) {
                 case "Upgrade":
                     int probability = (int) (Math.random() * 100) + 1; //1~100
                     if (probability > 90) {
-                        grade += 2;
+                        tmpGrade += 2;
                     } else if (probability > 60) {
-                        grade++;
+                        tmpGrade++;
                     } else if (probability <= 40) {
-                        grade--;
+                        tmpGrade--;
                     }
-                    if (grade < 0) {
-                        System.out.printf("현재등급 : %d\n", grade);
+                    characterItem.setGrade(tmpGrade);
+                    if (tmpGrade < 0) {
+                        System.out.printf("현재등급 : %d\n", tmpGrade);
                         System.out.println("실패");
-                        break forge;
-                    } else if (grade < 11) {
-                        System.out.printf("현재등급 : %d\n", grade);
+                        return;
+                    } else if (tmpGrade < 11) {
+                        System.out.printf("현재등급 : %d\n", tmpGrade);
                         System.out.print("Upgrade / Stop : ");
                         input = scan.nextLine();
                         break;
                     }
-                    System.out.printf("현재등급 : %d\n", grade);
+                    System.out.printf("현재등급 : %d\n", tmpGrade);
                     System.out.println("성공");
-                    break forge;
+                    return;
                 case "Stop":
                     System.out.println("강화를 종료합니다.");
-                    System.out.printf("최종등급 : %d\n", grade);
-                    break forge;
+                    System.out.printf("최종등급 : %d\n", tmpGrade);
+                    return;
                 default:
                     System.out.println("다시 입력해주세요.");
                     System.out.print("Upgrade / Stop : ");
                     input = scan.nextLine();
-                    break;
             }
         }
     }
 }
 
-class Forge{
-    public Forge(Item item){
+class Character {
+    private int money;
+    private Item inventory;
+    private final Scanner scan = new Scanner((System.in));
 
+    public Character() {
+        money = 3000;
+        System.out.printf("""
+                아래의 무기 중 하나를 선택해 주세요.
+                (LongSword / ShortSword / Bow )
+                입력창 :\s""");
+        String input = scan.nextLine();
+        while (true) {
+            switch (input) {
+                case "LongSword":
+                    inventory = new LongSword();
+                    return;
+                case "ShortSword":
+                    inventory = new ShortSword();
+                    return;
+                case "Bow":
+                    inventory = new Bow();
+                    return;
+                default:
+                    System.out.println("다시 입력해주세요.");
+                    input = scan.nextLine();
+            }
+        }
+    }
+
+    public Item getInventory() {
+        return inventory;
+    }
+    public void goForge(){
+        
     }
 }
 
-class User{
-    private int money;
-    private Item inventory;
-}
-class Item{
+class Item {
     private String category, name;
     private int grade;
-    private int probability;
 
-    public Item(String category){
+    public Item(String category) {
         this.category = category;
         grade = 1;
     }
-    public void setName(String name){
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
-    public String getCategory(){
+
+    public String getCategory() {
         return category;
     }
-    public String getName(){
-        return getName();
+
+    public String getName() {
+        return name;
     }
-    public int getGrade(){
+
+    public int getGrade() {
         return grade;
     }
 }
 
-class Sword extends Item{
-    public Sword(){
+class Sword extends Item {
+    public Sword() {
         super("Sword");
     }
 }
-class LongSword extends Sword{
-    public LongSword(){
+
+class LongSword extends Sword {
+    public LongSword() {
         setName("LongSword");
     }
 }
-class ShortSword extends Sword{
-    public ShortSword(){
+
+class ShortSword extends Sword {
+    public ShortSword() {
         setName("ShortSword");
     }
 }
-class Bow extends Item{
-    public Bow(){
+
+class Bow extends Item {
+    public Bow() {
         super("Bow");
         setName("Bow");
     }
