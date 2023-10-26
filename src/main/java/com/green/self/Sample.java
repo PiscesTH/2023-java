6405db96ae9eaccaf451266d118f2a290a11bbff
TH <kareme007@gmail.com>
2023-10-26 18:07:39 +0900

    23.10.26.

--- src/main/java/com/green/self/Sample.java
+++ src/main/java/com/green/self/Sample.java
@@ -1,17 +1,54 @@
 package com.green.self;
 
-class SampleTest{
-    private SampleTest(){}
-
-    public static SampleTest makeClass(){
-        return new SampleTest();
-    }
-}
-
+import java.util.Arrays;
 
 public class Sample {
     public static void main(String[] args) {
         //home test
-        SampleTest sp = SampleTest.makeClass();
+        int[] array = Solution.makeArr(Solution.getRandomLen());
+        Solution s = new Solution();
+        int answer = s.solution(array);
+        System.out.println(answer);
     }
 }
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        int[] numCnt = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            numCnt[array[i]]++;
+        }
+        int maxIdx = 0;
+        for (int i = 0; i < numCnt.length; i++) {
+            if (numCnt[maxIdx] < numCnt[i]) {
+                maxIdx = i;
+            }
+        }
+        for (int i = 0; i < numCnt.length; i++) {
+            if (numCnt[maxIdx] == numCnt[i] && maxIdx != i) {
+                return -1;
+            }
+        }
+        System.out.println(Arrays.toString(numCnt));
+        answer = maxIdx;
+        return answer;
+    }
+
+    public static int getRandomLen() {
+        return (int) (Math.random() * 10000);
+    }
+
+    public static int getRandomVal() {
+        return (int) (Math.random() * 10);
+    }
+
+    public static int[] makeArr(int len) {
+        int[] tmpArr = new int[len];
+        for (int i = 0; i < tmpArr.length; i++) {
+            tmpArr[i] = getRandomVal();
+        }
+        return tmpArr;
+    }
+}
+
