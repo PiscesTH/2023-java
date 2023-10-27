2edbd54d3e44f77eb1cb6df4ba5256358688b2ee
PiscesTH <kareme007@gmail.com>
2023-10-27 22:52:48 +0900

    23.10.27. Home

--- src/main/java/com/green/day20/ch8/ExceptionEx9.java
+++ src/main/java/com/green/day20/ch8/ExceptionEx9.java
@@ -6,7 +6,7 @@ public class ExceptionEx9 {
             Exception e = new Exception("일부러 에러 터트림");  //에러 의도적으로 만드는 법
             System.out.println("test");
             throw e;
-            //throw new Exception("일부러 에러 터트림");        한 줄로 처리 가능
+            //throw new Exception("일부러 에러 터트림");        한 줄로 작성 가능
         }catch (Exception e){
             System.out.println(e.getMessage());
             e.printStackTrace();
