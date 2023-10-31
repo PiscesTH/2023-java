782fd8cd40dde9e45210bb624b868710b9a31733
PiscesTH <kareme007@gmail.com>
2023-10-31 22:46:04 +0900

    23.10.31. Home

---
+++ src/main/java/com/green/self/SelfPrac13.java
@@ -0,0 +1,10 @@
+package com.green.self;
+
+import java.util.Arrays;
+
+//1,2,3,1 조합을 만들 수 있는 개수
+public class SelfPrac13 {
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 1, 2, 3, 1, 2, 3, 1};
+    }
+}
