54f4c9a96411a0413c9cdb01c6335cb81f1799d1
PiscesTH <kareme007@gmail.com>
2023-11-05 23:31:40 +0900

    23.11.05. Home

--- src/main/java/com/green/self/Memo.java
+++ src/main/java/com/green/self/Memo.java
@@ -1,5 +1,19 @@
 package com.green.self;
 
-public class Memo{
+import java.util.StringJoiner;
 
+public class Memo {
+    public static void main(String[] args) {
+        String animals = "dog,cat,bear";
+        String[] arr = animals.split(",");
+        String str = String.join("-", arr);
+        System.out.println(str);
+
+        StringJoiner sj = new StringJoiner(",", "(", ")");
+        String[] strArr = {"aaa", "bbb", "ccc"};
+        for (String s : strArr) {
+            sj.add(s);
+        }
+        System.out.println(sj.toString());
+    }
 }
