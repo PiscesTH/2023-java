c9f7315932b110923468782bd28b9ea0c7c937d0
TH <kareme007@gmail.com>
2023-11-29 10:52:16 +0900

    23.11.29.

--- src/main/java/com/green/day44/MyInfo.java
+++ src/main/java/com/green/day44/MyInfo.java
@@ -1,5 +1,18 @@
 package com.green.day44;
 
 public class MyInfo {
+    private String title;
 
+    public MyInfo title(String title){
+        this.title = title;
+        return this;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    @Override
+    public String toString(){
+        return String.format("title : %s", title);
+    }
 }
--- src/main/java/com/green/day44/Test.java
+++ src/main/java/com/green/day44/Test.java
@@ -2,6 +2,8 @@ package com.green.day44;
 
 public class Test {
     public static void main(String[] args) {
+        MyInfo mi = new MyInfo().title("ddd");
 
+        System.out.println(mi);
     }
 }
