9dc2824a39b07d29c226a1297bcef45ea74eca4b
TH <kareme007@gmail.com>
2024-01-29 11:04:46 +0900

    24.01.29.

--- src/main/java/com/green/day84/StreamEx3.java
+++ src/main/java/com/green/day84/StreamEx3.java
@@ -1,4 +1,16 @@
 package com.green.day84;
 
+import java.util.stream.Stream;
+
 public class StreamEx3 {
+    public static void main(String[] args) {
+        Stream.iterate(0, item -> item + 2)
+                //무한 스트림
+                .mapToInt(Integer::valueOf)
+                .skip(3)    //3개 skip
+                .limit(10)
+                .boxed()
+                .forEach(System.out::println);
+
+    }
 }
